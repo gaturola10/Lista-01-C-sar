20f10684b9495658e3eb0973b6abced9ffc13d19
gaturola10 <gacasilhas@gmail.com>
2024-09-09 22:25:28 -0300

    Add files via upload

---
+++ Ex2b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    char nome[20];
+
+    printf("Ola eu sou o N1, como eh seu nome?\n");
+    scanf("%s", &nome);
+    printf("Bem-vindo ao clube %s.", nome);
+
+}
