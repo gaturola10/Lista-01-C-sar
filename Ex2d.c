b08e892810b6473e3a71c9719d0888556a02f75f
gaturola10 <gacasilhas@gmail.com>
2024-09-09 22:34:33 -0300

    Add files via upload

--- Ex2d.c
+++ Ex2d.c
@@ -2,15 +2,23 @@
 
 int main(){
 
-    float raio;
+    float altura, largura;
 
-    printf("Digite o raio do circulo:");
-    scanf("%f", &raio);
+    printf("Digite a largura da parede em metros:");
+    scanf("%f", &largura);
+    printf("Digite a altura da parede em metros:");
+    scanf("%f", &altura);
 
-    float area = 3.14 * (raio*raio);
+    float m2 = altura * largura;
 
-    printf("A area do seu circulo eh de:%.2f", area);
+    int consumo = 300;
+    float nescessarioml = m2 * consumo;
+    int lata = 2000;
+    float totallatas = nescessarioml / lata;
+
+    printf("Voce vai prescisar de %0.2f latas de tinta." ,totallatas);
 
     return 0;
+
 
 }
